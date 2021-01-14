d320b77697e4d5d63a9d0fe404bd26483d14a82b
Marcelin <marsbob@live.fr>
2021-01-14 11:09:23 +0100

    partie 2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
         <h1>Exo1</h1>
+        <p className="textGreen">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias omnis optio ipsam architecto vero laboriosam repellendus cumque dolorem. Adipisci quia nemo eum quae soluta libero dolores molestiae quam saepe provident.</p>
     </div>
   );
 }
